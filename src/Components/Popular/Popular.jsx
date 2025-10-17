a8a4646f7b58fa5becf1970e6a5f58416ce8347c
lithin26 <lithinraswanth2003@gmail.com>
2025-10-17 17:26:54 +0530

    Update API endpoint for popular products

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -7,7 +7,8 @@ export const Popular = () => {
   const [popularProducts,setPopularProducts] = useState([]);
 
   useEffect(()=>{
-    fetch('http://localhost:4000/popularinwomen')
+    // fetch('http://localhost:4000/popularinwomen')
+    fetch('https://your-backend-api-name.onrender.com/popularinwomen')
     .then((response)=>response.json())
     .then((data)=>setPopularProducts(data));
   },[])
